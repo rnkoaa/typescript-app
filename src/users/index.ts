d4b811149ec20e291f28a8e146b78ae5230ef817
Richard Agyei <rnkoaa@users.noreply.github.com>
2022-11-02 00:57:03 +0000

    users

---
+++ src/users/index.ts
@@ -0,0 +1,28 @@
+
+export interface User {
+
+}
+
+export interface Address {
+
+}
+
+export interface Company {
+
+}
+
+export interface Comment {
+
+}
+
+export interface Album {
+
+}
+
+export interface Post {
+	
+}
+
+export interface Todo {
+	
+}
